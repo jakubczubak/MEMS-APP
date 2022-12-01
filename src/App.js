5e1bdd432aaab8f8d72b76a8d913e24384ff4b2a
Kuba <czubakjakub94@gmail.com>
2022-12-01 20:18:27 +0100

    added new route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Nav } from "./components/Nav";
 import { Footer } from "./components/Footer";
 import { Main } from "./components/Main";
@@ -10,7 +15,7 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Navigate replace to="/hot" />} />
           <Route path="/hot" element={<Main />} />
           <Route path="/regural" element={<Main />} />
         </Routes>
