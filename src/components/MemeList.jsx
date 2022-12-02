2c9373aa38d526cc06b1a1e36057ee769892b9ec
Kuba <czubakjakub94@gmail.com>
2022-12-02 11:26:35 +0100

    edited components

--- src/components/MemeList.jsx
+++ src/components/MemeList.jsx
@@ -1,3 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
 export function MemeList(props) {
+
+  const [list, setList] = useState([]);
+
+  
+
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await fetch('https://yourapi.com');
+      setList(data);
+    }
+  
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, [])
+
   return <div>Tu powstanie komponent listy memów {props.category}</div>;
 }
