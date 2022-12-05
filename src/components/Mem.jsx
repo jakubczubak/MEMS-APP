308985934c7b33126bdd7d73d8ddefa9fa4f6029
Kuba <czubakjakub94@gmail.com>
2022-12-05 20:30:14 +0100

    added new components

--- src/components/Mem.jsx
+++ src/components/Mem.jsx
@@ -1,15 +1,62 @@
-import styles from '../components/Mem.module.css'
+import styles from "../components/Mem.module.css";
+import { useState } from "react";
 
 export function Mem(props) {
+  const [upVotes, setUpVotes] = useState(props.mem.upvotes);
+  const [downVotes, setDownVotes] = useState(props.mem.downvotes);
 
-    return <div className={styles.mem_wrapper}>
-    <div className={styles.mem}>
-        <h3 className={styles.mem_title}>Title</h3>
-        <img src="" alt="" />
+  function incrementUpVotes() {
+    setUpVotes(upVotes + 1);
+    updateMemVotes(props.mem.id);
+  }
+
+  function incrementDownVotes() {
+    setDownVotes(downVotes + 1);
+    updateMemVotes(props.mem.id);
+  }
+
+  
+
+  async function updateMemVotes(id) {
+    const mem = await (await fetch("http://localhost:4000/mems/" + id)).json();
+
+    mem.upvotes += 1;
+    mem.downvotes += 1;
+
+    fetch("http://localhost:4000/mems/" + id, {
+      method: "PUT", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mem),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  return (
+    <div className={styles.mem_wrapper}>
+      <div className={styles.mem}>
+        <h3 className={styles.mem_title}>{props.mem.title}</h3>
+        <img
+          className={styles.mem_img}
+          src={require("../assets/" + props.mem.img)}
+          alt="mem"
+        />
+      </div>
+      <div className={styles.mem_btn_wrapper}>
+        <button className={styles.mem_btn} onClick={incrementUpVotes}>
+          👍 {upVotes}
+        </button>
+        <button className={styles.mem_btn} onClick={incrementDownVotes}>
+          👎 {downVotes}
+        </button>
+      </div>
     </div>
-    <div className={styles.mem_btn_wrapper}>
-        <button className={styles.mem_btn}>👍</button>
-        <button className={styles.mem_btn}>👎</button>
-    </div>
-  </div>
+  );
 }
--- src/components/MemeList.jsx
+++ src/components/MemeList.jsx
@@ -1,27 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import styles from './MemeList.module.css'
-import { Mem } from './Mem'
+import React, { useState, useEffect } from "react";
+import styles from "./MemeList.module.css";
+import { Mem } from "./Mem";
 
 export function MemeList(props) {
-
   const [list, setList] = useState([]);
+  const [hotList, setHotList] = useState([]);
+  const [reguralList, setReguralList] = useState([]);
 
   useEffect(() => {
     // declare the data fetching function
     const fetchData = async () => {
-      const data = await fetch('https://yourapi.com');
+      const data = await (await fetch("http://localhost:4000/mems")).json();
       setList(data);
-    }
-  
+    };
+
     // call the function
     fetchData()
       // make sure to catch any error
       .catch(console.error);
-  }, [])
+  }, []);
 
-  return <main>
-    <Mem />
-    <Mem />
-    <Mem />
-    </main>;
+  return (
+    <main>
+      {list &&
+        list.map((mem) => {
+          return <Mem mem={mem} key={mem.id} />;
+        })}
+    </main>
+  );
 }
